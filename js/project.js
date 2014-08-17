f666933e9282a9418d9c13da661783a17a9c0838
Balaji Athreya <athreya86@gmail.com>
2014-08-16 20:25:52 -0700

    geolocation stuff

--- js/project.js
+++ js/project.js
@@ -2,30 +2,45 @@ angular.module('tweetMapModule', ['google-maps'])
 .controller('TweetMapController', ['$scope', function ($scope) {
   // Enable the new Google Maps visuals until it gets enabled by default.
   // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
-  // google.maps.visualRefresh = true;
+  google.maps.visualRefresh = true;
+
+  // default latitude and longitude values. kinda at the center of the global
+  // in the north pacific ocean
+  $scope.lat = 45.521071299999996;
+  $scope.lon = -122.88757439999998;
+
+  var setGeoLocation = function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position){
+            $scope.lat = position.coords.latitude;
+            $scope.lon = position.coords.longitude;
+          },
+          function( error ){
+            console.log( "Something went wrong: ", error );
+          },
+          {}
+        );
+    }    
+  };
 
   $scope.map = {
-    center: {
-        latitude: 45,
-        longitude: -73
+    center: {        
+        latitude: $scope.lat,
+        longitude: $scope.lon
     },
-    zoom: 8,
+    zoom: 3,
     events : {
       'click': function (mapModel, eventName, originalEventArgs) {
         var e = originalEventArgs[0];
         var lat = e.latLng.lat(),
         lon = e.latLng.lng();
-        $scope.map.clickedMarker = {
-          id: 0,
-          title: 'You clicked here ' + 'lat: ' + lat + ' lon: ' + lon,
-          latitude: lat,
-          longitude: lon
-        };
+        // make service call here
         //scope apply required because this event handler is outside of the angular domain
         $scope.$apply();
-        console.log('latitude : '+lat);
-        console.log('longitude : '+lon);
       }    
     }
-  }
+  };
+
+  setGeoLocation();
 }]);
