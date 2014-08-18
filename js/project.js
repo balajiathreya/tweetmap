5230f9cdbc29dc03b3eba40d83cd6a0785039500
Balaji Athreya <athreya86@gmail.com>
2014-08-17 17:59:23 -0700

    initial commit with geo stuff

--- js/project.js
+++ js/project.js
@@ -1,13 +1,18 @@
 angular.module('tweetMapModule', ['google-maps'])
-.controller('TweetMapController', ['$scope', function ($scope) {
+.controller('TweetMapController', ['$scope','$http', function ($scope,$http) {
   // Enable the new Google Maps visuals until it gets enabled by default.
   // See http://googlegeodevelopers.blogspot.ca/2013/05/a-fresh-new-look-for-maps-api-for-all.html
   google.maps.visualRefresh = true;
+  
+  // defaults to a locale which is global
+  DEFAULT_WOEID = 1;
 
   // default latitude and longitude values. kinda at the center of the global
   // in the north pacific ocean
   $scope.lat = 45.521071299999996;
   $scope.lon = -122.88757439999998;
+  $scope.trending = '';
+  $scope.selectedLocale = '';
 
   var setGeoLocation = function(){
     if (navigator.geolocation) {
@@ -24,20 +29,48 @@ angular.module('tweetMapModule', ['google-maps'])
     }    
   };
 
+  var setTrendingData = function(woeid){
+    url = "http://localhost:5000/gettrending?woeid="+$scope.woeid;
+    var responsePromise = $http.get(url);
+    console.log('woeid '+woeid);
+    responsePromise.success(function(data, status, headers, config) {      
+      $scope.trending = data;
+      console.log('trending data: ' + data);
+    });
+
+    responsePromise.error(function(data, status, headers, config) {
+      $scope.trending = data;
+    });  
+  };
+
   $scope.map = {
     center: {        
         latitude: $scope.lat,
         longitude: $scope.lon
     },
+    options: { draggable: true },
     zoom: 3,
     events : {
       'click': function (mapModel, eventName, originalEventArgs) {
         var e = originalEventArgs[0];
         var lat = e.latLng.lat(),
         lon = e.latLng.lng();
+        url = "http://localhost:5000/getWOEID?lat="+lat+'&lon='+lon
+        var responsePromise = $http.get(url);
 
-        console.log('lat '+lat);
-        console.log('lon '+lon);
+        responsePromise.success(function(data, status, headers, config) {
+          $scope.trending = data;
+          $scope.selectedLocale = data.places.place[0].name;
+          woeid = data.places.place[0].woeid;          
+          setTrendingData(woeid);
+        });
+
+        responsePromise.error(function(data, status, headers, config) {
+          $scope.trending = data;
+          $scope.selectedLocale = "Couldn't get locale";
+          setTrendingData(DEFAULT_WOEID);
+        });
+        
         // make service call here
         //scope apply required because this event handler is outside of the angular domain
         $scope.$apply();
